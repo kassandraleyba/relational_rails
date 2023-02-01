6d960944325e753082ee39970a7ad7615e459c18
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-31 21:07:09 -0700

    Add welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/welcome', to: 'welcome#index'
   get '/artist', to: 'artist#index'
   get '/works', to: 'works#index'
 end
