b3857720ba270b3ecfd3cc9382fe6601f06731ec
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-06 16:02:57 -0700

    Add user story 14

--- spec/features/works/edit_spec.rb
+++ spec/features/works/edit_spec.rb
@@ -26,33 +26,38 @@ RSpec.describe 'Works Page' do
         describe 'Then I see a link to update that Work "Update Work"' do
           describe 'When I click the link' do
             it 'I am taken to /works/:id/edit where I see a form to edit the works attributes:' do
-                visit "/artists"
-                
-                click_button("Edit #{artist_1.name}")
-                # click_button("Edit")
-                
-                expect(current_path).to eq("/artists/#{artist_1.id}/edit")
-              end
+              visit "/works"
+              
+              click_button("Edit #{work_1.title}")
+              # click_button("Edit")
+              
+              expect(current_path).to eq("/works/#{work_1.id}/edit")
             end
           end
         end
       end
     end
 
-    # describe 'When I click the button to submit the form "Update Works"' do
-    #   describe 'Then a `PATCH` request is sent to /works/:id,' do
-    #     describe 'the works data is updated,' do
-    #       it 'and I am redirected to the Work Show page where I see the Works updated information' do
-    #           visit "/artists"
-              
-    #           click_button("Edit #{artist_1.name}")
-    #           # click_button("Edit")
-              
-    #           expect(current_path).to eq("/artists/#{artist_1.id}/edit")
-    #         end
-    #       end
-    #     end
-    #   end
-    # end
+    describe 'When I click the button to submit the form "Update Works"' do
+      describe 'Then a `PATCH` request is sent to /works/:id,' do
+        describe 'the works data is updated,' do
+          it 'and I am redirected to the Work Show page where I see the Works updated information' do
+            visit "/works"
+        
+            expect(page).to have_content("Glass Tear")
+        
+            click_button "Edit Glass Tear"
+            # click_button "Edit"
+        
+            fill_in "Title", with: "Glass Tears"
+            find('#available_for_purchase').click
+            click_button "Update Photograph"
+        
+            expect(current_path).to eq("/works")
+            expect(page).to have_content("Glass Tears")
+          end
+        end
+      end
+    end
   end
 end
